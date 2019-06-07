2709d3376cc2d1ba31422e6db522f63df2bd6776
loudbyte <47244117+loudbyte@users.noreply.github.com>
2019-06-08 04:08:33 +0600

    SimpleDotTestDrive

---
+++ LearningProject/src/SimpleDotCom/SimpleDotComTestDrive.java
@@ -0,0 +1,26 @@
+package SimpleDotCom;
+
+public class SimpleDotComTestDrive {
+	
+	public static void main (String[] args) {
+		
+		SimpleDotCom dot = new SimpleDotCom();
+		
+		int[] locations = {2,3,4};
+		
+		dot.setLocationCells(locations);
+		
+		String userGuess = "2";
+		String result = dot.checkYpurself(userGuess);
+		String testResult = "Неудача";
+		if (result.equals("Попал")) {
+			
+			testResult = "Пройден";
+			
+		}
+		
+		System.out.println(testResult);
+		
+	}
+
+}
