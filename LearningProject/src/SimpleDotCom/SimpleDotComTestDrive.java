00d7e1e7d5e45301592f036e927be4130aedd047
loudbyte <47244117+loudbyte@users.noreply.github.com>
2019-06-08 16:27:00 +0600

    added class SimpleDotCom.java

--- LearningProject/src/SimpleDotCom/SimpleDotComTestDrive.java
+++ LearningProject/src/SimpleDotCom/SimpleDotComTestDrive.java
@@ -5,13 +5,10 @@ public class SimpleDotComTestDrive {
 	public static void main (String[] args) {
 		
 		SimpleDotCom dot = new SimpleDotCom();
-		
 		int[] locations = {2,3,4};
-		
 		dot.setLocationCells(locations);
-		
 		String userGuess = "2";
-		String result = dot.checkYpurself(userGuess);
+		String result = dot.checkYourself(userGuess);
 		String testResult = "Неудача";
 		if (result.equals("Попал")) {
 			
@@ -22,5 +19,4 @@ public class SimpleDotComTestDrive {
 		System.out.println(testResult);
 		
 	}
-
 }
